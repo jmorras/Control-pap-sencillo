318a473dcbe6c2a1f8e9246c2ab1305605baf80a
Jorge Morrás Github <jmorras@users.noreply.github.com>
2017-02-18 17:32:08 +0100

    Update MotorX_manual_con_cncshield.ino

--- MotorX_manual_con_cncshield.ino
+++ MotorX_manual_con_cncshield.ino
@@ -10,12 +10,16 @@
 // Cuando ha mandado un número de pulsos, cambia la dirección
 
 
-int EnPin = 8;     // cncshiel conecta la señal de enable de los 4 pololus al pin 8
-int Dir_Pin_X = 5; // cncshiel conecta la señal de dirección del motor X al pin 5
-int Step_Pin_X = 2;  // cncshiel conecta la señal de paso del motor X al pin 2
+int EnPin = 8;     // cncshield conecta la señal de enable de los 4 pololus al pin 8
+int Dir_Pin_X = 5; // cncshield conecta la señal de dirección del motor X al pin 5
+int Step_Pin_X = 2;  // cncshield conecta la señal de paso del motor X al pin 2
 
 bool Dir_estado = LOW;   // Esta variable sirve para cambiar de dirección
 int contador = 0;
+
+int Tempsensor = 9; // ping para poner el sensor de temperatura
+
+
 void setup() {
   pinMode(EnPin, OUTPUT);        // Define el pin como salida
   pinMode(Dir_Pin_X, OUTPUT);    // Define el pin como salida
@@ -23,6 +27,9 @@ void setup() {
   
   digitalWrite(EnPin, LOW);      // El pololu se activa con nivel bajo
   digitalWrite(Dir_Pin_X, Dir_estado);   // Saca por la pata de dircción el estado inicial
+  
+  //configurar pin de sensor temperatura
+  
 }
 
 void loop() {                    // Para mandar un paso tenemos que mandar un 1 y luego un 0
@@ -35,5 +42,13 @@ void loop() {                    // Para mandar un paso tenemos que mandar un 1 
     Dir_estado = !Dir_estado;    // Invierte la direccion
     contador=0;                  // Reinicia el contador para hacer otros pasos en dirección contraria
     digitalWrite(Dir_Pin_X, Dir_estado);    // Saca por el pin de dirección el estado de la dirección
+    if LeeTemp() >50 then
+      end;  // no recuerdo si hay que poner end ó exit().
+    
+  }
+  
+  int LeeTemp()
+  {// codigo para leer el sensor de temperatura
+    
   }
 }
